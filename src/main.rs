ba3fd3ffefbd4f797e18b7db7fb843530a748eb2
Jonathan Windle <jonathanwindle@gmail.com>
2020-06-17 20:40:25 -0700

    Remove testing code from main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,20 @@
-use std::io::Read;
-use std::net::{Shutdown, TcpListener, TcpStream};
-use std::thread;
+// Copyright 2020 Jonathan Windle
+
+// This file is part of Platform.
+
+// Platform is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Platform is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with Platform.  If not, see <https://www.gnu.org/licenses/>.
 
 mod irc;
 
-fn handle_client(mut stream: TcpStream) {
-    let mut request = irc::request::new();
-    while match stream.read(request.data()) {
-        Ok(_size) => {
-            if request.valid() {
-                for message in request.messages() {
-                    println!("String:    {:?}", message.string());
-                    println!("Command:   {:?}", message.command());
-                    for p in message.parameters() {
-                        println!("Parameter: {:?}", p);
-                    }
-                }
-            }
-            request.clear_data();
-
-            true
-        }
-        Err(_) => {
-            println!(
-                "An error occurred, terminating connection with {}",
-                stream.peer_addr().unwrap()
-            );
-            stream.shutdown(Shutdown::Both).unwrap();
-            false
-        }
-    } {}
-}
-
-fn main() {
-    let listener = TcpListener::bind("0.0.0.0:6667").unwrap();
-    // accept connections and process them, spawning a new thread for each one
-    println!("Server listening on port 6667");
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                println!("New connection: {}", stream.peer_addr().unwrap());
-                thread::spawn(move || {
-                    // connection succeeded
-                    handle_client(stream)
-                });
-            }
-            Err(e) => {
-                println!("Error: {}", e);
-                /* connection failed */
-            }
-        }
-    }
-    // close the socket server
-    drop(listener);
-}
+fn main() {}
